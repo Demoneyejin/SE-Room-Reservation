c2312581de031dcace492c2ef1bfa4320b09d7de
DemonAlecks <alex.batista0@gmail.com>
2019-10-15 14:36:42 -0400

    Adding simple User class from the model. I'm thinking we're not going to store the password here if this is going to be the user stored on the Database. We'll have to update the model when I get further and see how password protection kicks in.

---
+++ Backend/reservo/src/main/java/com/reservo/reservo/Models/User.java
@@ -0,0 +1,41 @@
+package com.reservo.reservo.Models;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User{
+
+    @Id
+    private String userId;
+    private String userName;
+    private Map<String, String> userSettings = new HashMap<>();
+
+    
+    public final String getUserID(){
+        return userId;
+    }
+    public final String getUserName(){
+        return userName;
+    }
+    //UpdateUserName() from the model
+    public void setUserName(final String name){
+        userId = name;
+    }
+    //Required UpdateUserID in the model
+    public void setUserID(final String Id){
+       userId = Id;
+    }
+
+    public final Map<String, String> getUserSettings(){
+        return userSettings;
+    }
+    public void setUserSettings(final Map<String, String> userSetting){
+        this.userSettings = userSetting;
+    }
+}
