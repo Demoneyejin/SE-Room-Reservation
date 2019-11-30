f8ca8ca40092975e313e2752df7f0c06c8cb4f73
Alex Batista <abati030@fiu.edu>
2019-11-30 00:52:10 -0500

    Updated so the API could generate the requests more vividly. Successfully tested with a local API tester to create the user. Have a weird issue in where username was null though.

--- Backend/reservo/src/main/java/com/reservo/reservo/Models/User.java
+++ Backend/reservo/src/main/java/com/reservo/reservo/Models/User.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Users")
+@Document
 public class User{
 
     @Id
